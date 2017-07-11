3b11a214f2e06239aff45f61e6e63d4ac6c54803
Jock Perkins <jockperkins@jocks-mbp.lsm.local>
2017-07-11 11:30:09 +0100

    * removed twig because who needs that?

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,10 +2,6 @@
 // Get autoloader
 require __DIR__ . '/../vendor/autoload.php';
 
-// Initialise twig
-$loader = new Twig_Loader_Filesystem(__DIR__ . '/../src');
-$twig = new Twig_Environment($loader);
-
 // Get tests
 require __DIR__ . '/../functions/PigLatin.php';
 require __DIR__ . '/../functions/Numbers.php';
